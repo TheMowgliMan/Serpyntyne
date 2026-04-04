e9c74c07a54eeefccf9317c1283149c94a3081f3
Caleb Reeves <calebreeves@fortune-cookie.lcl>
2026-04-04 08:53:05 -0400

    Interrupts working x2

--- kernel/arch/x86_64/interrupts.c
+++ kernel/arch/x86_64/interrupts.c
@@ -1,10 +1,25 @@
 #include <interrupts.h>
 #include <terminal.h>
 #include <stdint.h>
+#include <memory.h>
 
 void handle_interrupt(struct intframe* infr)
 {
-    kputs("Interrupt recieved:\r\n");
+    char c[64];
+    uint8_t type = 0;
+    if (infr->vector < 32)
+    {
+        char msg[] = "Exception";
+        memcpy(&c, &msg, sizeof(char) * strlen(msg));
+        type = 1;
+    }
+    else
+    {
+        char msg[] = "Interrupt";
+        memcpy(&c, &msg, sizeof(char) * strlen(msg));
+    }
+
+    kprintf("%s recieved:\r\n", c);
     kprintf("Vector: %x \r\n", (int64_t)infr->vector);
     asm volatile ("sti");
 }
