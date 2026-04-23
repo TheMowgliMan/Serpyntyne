59606b0b36885bc2900c23e0086095beb2bd46df
Caleb Reeves <calebreeves@fortune-cookie.lcl>
2026-04-23 18:24:47 -0400

    stuph

--- kernel/arch/x86_64/paging.c
+++ kernel/arch/x86_64/paging.c
@@ -17,6 +17,7 @@ int map_page(struct pagemap *map, uintptr_t virt_addr, struct physFrame phys_add
     {
         struct physFrame new_frame = allocate_page_generic(4);
         map->top_level[pml4i] = (uint64_t)(new_frame.phys_addr & ~PAGE_TABLE_ADDRESS_MASK);
+
         set_table_flags(&map->top_level[pml4i], PAGE_DIRECTORY_PRESENT | PAGE_DIRECTORY_READWRITE | PAGE_DIRECTORY_USERSUPERVISOR);
     }
 
@@ -25,6 +26,7 @@ int map_page(struct pagemap *map, uintptr_t virt_addr, struct physFrame phys_add
     {
         struct physFrame new_frame = allocate_page_generic(6);
         pml3v[pml3i] = (uint64_t)(new_frame.phys_addr & ~PAGE_TABLE_ADDRESS_MASK);
+
         set_table_flags(&pml3v[pml3i], PAGE_DIRECTORY_PRESENT | PAGE_DIRECTORY_READWRITE | PAGE_DIRECTORY_USERSUPERVISOR);
     }
 
@@ -33,6 +35,7 @@ int map_page(struct pagemap *map, uintptr_t virt_addr, struct physFrame phys_add
     if (phys_addr.size == LARGE_PAGE_SIZE_EXPONENT)
     {
         pml2v[pml2i] = (uint64_t)(phys_addr.phys_addr & ~PAGE_TABLE_ADDRESS_MASK);
+
         set_table_flags((uint64_t*)(&pml2v[pml2i]), PAGE_DIRECTORY_SIZE_BIT | (uint16_t)(flags & 0xFFFF));
         if (!(flags & MAP_EXECUTABLE)) set_table_nx((uint64_t*)(pml2v[pml2i]));
 
@@ -44,6 +47,7 @@ int map_page(struct pagemap *map, uintptr_t virt_addr, struct physFrame phys_add
     {
         struct physFrame new_frame = allocate_page_generic(6);
         pml2v[pml2i] = (uint64_t)(new_frame.phys_addr & ~PAGE_TABLE_ADDRESS_MASK);
+
         set_table_flags(&pml2v[pml2i], PAGE_DIRECTORY_PRESENT | PAGE_DIRECTORY_READWRITE | PAGE_DIRECTORY_USERSUPERVISOR);
     }
 
